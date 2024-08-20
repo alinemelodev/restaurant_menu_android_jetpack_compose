b382109f88a451027ab9e9c888b279269dfd8810
aline <alinecarvalho.pro@gmail.com>
2024-08-20 13:39:40 -0300

    feat: implementing states

--- AlineFood/app/src/main/java/com/aline/alinefood/ui/screens/HomeScreen.kt
+++ AlineFood/app/src/main/java/com/aline/alinefood/ui/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.aline.alinefood.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
@@ -7,7 +8,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,6 +38,13 @@ fun HomeScreen() {
             TitleMenu()
             Spacer(Modifier)
         }
+        item {
+            var text by remember {mutableStateOf("")}
+            OutlinedTextField(value = text, onValueChange = { newValue ->
+                text = newValue
+            })
+
+        }
         items(sections) { section ->
             ProductSection(title = section.title, products = section.products)
         }
@@ -41,6 +54,10 @@ fun HomeScreen() {
         }
     }
 }
+
+
+
+
 
 @Preview(showSystemUi = true)
 @Composable
